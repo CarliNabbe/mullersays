c9a7e506e9c3a4760c2d36e3287473967a62a2bb
CarliNabbe <44226700+CarliNabbe@users.noreply.github.com>
2019-04-05 21:45:45 +0200

    Add files via upload
    Added new javascript functionalities and sounds

--- js/app.js
+++ js/app.js
@@ -1,42 +1,43 @@
 let start = document.getElementById("start");
 let audio = document.getElementById("audio");
-let sounds = ["./sounds/bmo.wav", "./sounds/linux.wav", "./sounds/check.wav", "./sounds/play.wav"];
+let sounds = ["./sounds/simonSound1.wav", "./sounds/simonSound2.wav", "./sounds/simonSound3.wav", "./sounds/simonSound4.wav"];
 let imagesArray = ["img/green.png", "img/red.png", "img/blue.png", "img/yellow.png"];
 let smoothScroll = document.getElementById("smoothScroll");
+let startMessage = document.getElementById("startLi");
+let simonImage = document.getElementById("simonSays");
 
 
-start.addEventListener("click", function(){
-    console.log("Click!");
-    startLi.innerHTML = "The game has started!";
-    if (startLi.style.display === "block") {
-        startLi.style.display = 'none';
-        simonSays.src = "img/simonsays.png";
+start.addEventListener('click', function(){
+    // console.log("Click!");
+    startGame();
 
+    startMessage.innerHTML = "The game has started!";
+    if (startMessage.style.display === "block") {
+        startMessage.style.display = 'none';
     }
     else {
-        startLi.style.display = "block";
-        simonSays.src = "img/start.png";
-        // audio.src = sounds[3];
-        // audio.play();
+        startMessage.style.display = "block";
+        simonImage.src = "img/start.png";
+        audio.src = sounds[3];
+        audio.play();
     }
-    for(let i = 0; i < 10; i++) {
-        randomImage();
-    }
-
-
 });
 
 function randomImage() {
+
     let num = Math.floor(Math.random() * 4); // 0...3
-    simonSays.src = imagesArray[num];
+    console.log(num);
+    simonImage.src = imagesArray[num];
     audio.src = sounds[num];
     audio.play();
+}
 
-    // setTimeout(() => {
-    //     randomImage()
-    // }, 2000);
-
-
+function startGame(){
+    for(let i=0; i<10; i++) {
+        setTimeout(() => {
+            randomImage()
+        }, 500*(i+1));
+    }
 }
 
 // Function smooth scroll
